71f3e87a9b605d63a7bbfb4a7d1a21eeb87aabfa
Sam Nicholls <samstudio8@gmail.com>
2022-02-06 15:52:02 +0000

    Break out test_secrets

--- tests/test_ffurf.py
+++ tests/test_ffurf.py
@@ -27,26 +27,10 @@ def tiered_ffurf():
 
 
 @pytest.fixture
-def secret_ffurf():
-    ffurf = FfurfConfig()
-    ffurf.add_config_key("my-secret", secret=True)
-    ffurf.set_config_key("my-secret", "hoot")
-    return ffurf
-
-
-@pytest.fixture
 def fill_ffurf():
     ffurf = FfurfConfig()
     ffurf.add_config_key("my-str")
     ffurf.add_config_key("my-int", key_type=int)
-    return ffurf
-
-
-@pytest.fixture
-def partial_secret_ffurf():
-    ffurf = FfurfConfig()
-    ffurf.add_config_key("my-secret", partial_secret=4)
-    ffurf.set_config_key("my-secret", "thisisverysecrethoot")
     return ffurf
 
 
@@ -274,41 +258,6 @@ def test_get_clean_keyerror(basic_ffurf):
 def test_get_keyconf_keyerror(basic_ffurf):
     with pytest.raises(KeyError):
         basic_ffurf.get_keyconf("no-key")
-
-
-def test_secret_get_reveals_key(secret_ffurf):
-    assert secret_ffurf.get("my-secret") == "hoot"
-
-
-def test_secret_getitem_reveals_key(secret_ffurf):
-    assert secret_ffurf["my-secret"] == "hoot"
-
-
-def test_secret_print_hides_key(secret_ffurf):
-    assert "hoot" not in str(secret_ffurf)
-    assert "********" in str(secret_ffurf)
-
-
-def test_secret_get_clean_hides_key(secret_ffurf):
-    assert secret_ffurf.get_clean("my-secret") == "********"
-
-
-def test_partial_secret_get_reveals_key(partial_secret_ffurf):
-    assert partial_secret_ffurf.get("my-secret") == "thisisverysecrethoot"
-
-
-def test_partial_secret_getitem_reveals_key(partial_secret_ffurf):
-    assert partial_secret_ffurf["my-secret"] == "thisisverysecrethoot"
-
-
-def test_partial_secret_print_hides_key(partial_secret_ffurf):
-    assert "hoot" in str(partial_secret_ffurf)
-    assert "thisisverysecret" not in str(partial_secret_ffurf)
-    assert "********hoot" in str(partial_secret_ffurf)
-
-
-def test_partial_secret_get_clean_hides_key(partial_secret_ffurf):
-    assert partial_secret_ffurf.get_clean("my-secret") == "********hoot"
 
 
 def test_getitem(basic_ffurf):
